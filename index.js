a2da4d28c659a713dd7cb055cb7bd60ed7870fce
PLTytus <34415908+PLTytus@users.noreply.github.com>
2023-01-24 11:03:37 +0100

    joaat (PHP -> JS)

--- index.js
+++ index.js
@@ -9,6 +9,52 @@ const TRANSLATE = {
 	hex: "hex",
 };
 
+function int32(i){
+	return i & 0xFFFFFFFF;
+}
+
+function signedInt32(i){
+	return i | (-(i & 0x80000000));
+}
+
+function signedInt8(i){
+	return i | (-(i & 0x80));
+}
+
+function unsignedInt(i){
+	return i >>> 0;
+}
+
+function hex(i){
+	return i.toString(16).toUpperCase();
+}
+
+function joaat(s, c=null, m=1/*1|-1*/){
+	s = unescape(encodeURIComponent(s.toLowerCase()));
+	
+	var hash = 0;
+
+	for(let i = 0; i < s.length; i++){
+		hash = int32(hash + signedInt8(s.charCodeAt(i)));
+		hash = int32(hash + (hash << 10));
+		hash = int32(hash ^ (hash >>> 6));
+	}
+
+	hash = int32(hash + (hash << 3));
+	hash = int32(hash ^ (hash >>> 11));
+	hash = int32(hash + (hash << 15));
+
+	if(c !== null) hash = int32(hash + joaat(c).signed * m);
+
+	let uint = unsignedInt(hash);
+
+	return {
+		signed: hash,
+		unsigned: uint,
+		hex: hex(uint),
+	};
+}
+
 document.onreadystatechange = function(){
 	document.querySelectorAll("input[type=text]").forEach(input => {
 		input.addEventListener("input", function(){
@@ -26,31 +72,19 @@ document.onreadystatechange = function(){
 
 		document.querySelectorAll(".result").forEach(x => x.innerHTML = "");
 
-		let xhr = new XMLHttpRequest();
+		let s = this.string.value.trim();
+		let c = this.category.value.trim();
 
-		xhr.open("POST", "joaat.php", true);
-		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		xhr.setRequestHeader("Accept", "application/json");
-		xhr.send(`string=${this.string.value.trim()}&category=${this.category.value}`);
-		xhr.onreadystatechange = function(){
-			if(this.readyState === 4) {
-				let response;
+		result = {
+			string: joaat(s),
+			plus: joaat(s, c, 1),
+			minus: joaat(s, c, -1),
+			category: joaat(c),
+		};
 
-				try {
-					response = JSON.parse(this.response);
-				} catch(_){
-					response = false;
-				}
-
-				if(response){
-					document.querySelectorAll(".result").forEach(x => {
-						let t = x.id.split('_');
-						x.innerHTML = response[TRANSLATE[t[1]]][TRANSLATE[t[0]]];
-					});
-				} else {
-					document.querySelectorAll(".result").forEach(x => x.innerHTML = "");
-				}
-			}
-		}
+		document.querySelectorAll(".result").forEach(x => {
+			let t = x.id.split('_');
+			x.innerHTML = result[TRANSLATE[t[1]]][TRANSLATE[t[0]]];
+		});
 	});
 }
